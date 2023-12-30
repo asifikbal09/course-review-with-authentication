6b146d27456eee04e8371e657642946547e715a4
asifikbal09 <asif.ikbal662@gmail.com>
2023-12-30 16:46:16 +0600

    [DONE] Final final commit.

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -4,20 +4,7 @@ import { UserRoleEnum } from './user.constant';
 const userValidationSchema = z.object({
   body: z.object({
     username: z.string({ required_error: 'username is required.' }),
-    password: z
-      .string({ required_error: 'password is required.' })
-      .refine((password) => /[A-Z]/.test(password), {
-        message: 'Password must contain at least one uppercase letter',
-      })
-      .refine((password) => /[a-z]/.test(password), {
-        message: 'Password must contain at least one lowercase letter',
-      })
-      .refine((password) => /\d/.test(password), {
-        message: 'Password must contain at least one digit',
-      })
-      .refine((password) => /[!@#$%^&*()_+{}[\]:;<>,.?~\\/-]/.test(password), {
-        message: 'Password must contain at least one special character',
-      }),
+    password: z.string({ required_error: 'password is required.' }),
     email: z.string({ required_error: 'email is required.' }),
     role: z.enum(UserRoleEnum as [string, ...string[]]).default('user'),
   }),
